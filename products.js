c0b7f1268d4882242b26072335ee1ec0c555050f
Manish CSLabs <manish@cslabs.ai>
2022-10-03 13:13:33 +0530

    add pdoducts.js

---
+++ products.js
@@ -0,0 +1,55 @@
+products = [
+  {
+    id: 0,
+    name: "PANDA Creation Adjustable Musical Walker (Pink)",
+    affiliateLink: "https://amzn.to/3SvB9mS",
+    img: "https://m.media-amazon.com/images/I/41xQYDdJdJL._SX300_SY300_QL70_FMwebp_.jpg",
+    descriptions: [
+      "Animal shape walker, adjustable height",
+      "Rattles, electronic musical",
+      "Cosy seat with back res",
+    ],
+  },
+  {
+    id: 1,
+    name: "Crucial MX500 250GB SATA 2.5-inch 7mm Internal SSD (CT250MX500SSD1)",
+    affiliateLink: "https://amzn.to/3rmKDot",
+    img: "https://m.media-amazon.com/images/I/81rg-38AdJL._SL1500_.jpg",
+    descriptions: [
+      "250GB, 6 Gigabits Per Second. SSD Endurance (TBW) - 100TB",
+      "2.5 Inch/SATA 6.0 Gb/s Interface, Compatible Device :Laptop and Desktop",
+      "Sequential reads/writes up to 560/510 MB/s and random reads/writes up to 95k/90k on all file types",
+      "Sequential reads/writes up to 560/510 MB/s and random reads/writes up to 95k/90k on all file types",
+      "Integrated Power Loss Immunity preserves all your saved work if the power unexpectedly gets cut",
+      "AES 256-bit hardware-based encryption keeps data safe and secure from hackers and thieves",
+      "5 Years Brand Warranty",
+    ],
+  },
+  {
+    id: 2,
+    name: "Reusable Metal Straws in Jute Pouch for Kids & Adults (2 Straight & 2 Bent, Silver)",
+    affiliateLink: "https://amzn.to/3dZCRxK",
+    img: "https://m.media-amazon.com/images/I/918RxUEhKRL._SL1500_.jpg",
+    descriptions: [
+      "REUSABLE METAL STRAWS: Our metal straws offer an eco-friendly alternative to plastic straws.",
+      "A pack of 4 comes with two bent and two straight silver straws along with a cleaning brush — use, clean, and repeat as many times as you want.",
+      "LIGHTWEIGHT STAINLESS STEEL: Made from 304 stainless steel, our metal straws — food-grade and BPA-free — weigh just 7 grams each. They are not only portable but also durable and dishwasher-friendly.",
+      "PERFECT FOR ALL OCCASIONS: Our metal straws blend seamlessly with table decor and are great for travelling as well as parties. You can enjoy your smoothie, bubble tea, juice, etc. anywhere with ease.",
+    ],
+  },
+  {
+    id: 3,
+    name: "KETOSTICS-WILSON-HARMONICA-SILVER-CHROME)",
+    affiliateLink: "https://amzn.to/3Csv4lC",
+    img: "https://m.media-amazon.com/images/I/71qpzeBq5CL._SL1500_.jpg",
+    descriptions: [
+      "10 Holes, Key Of C",
+      "Lets you playing music any time, anywhere",
+      "The reed plate with electroplating antirust, the hole, reed gap is better after electroplating. At the same time, you will feel comfortable when playing,easy to use the skill,such as over-blowing and bending, giving the performer a new experience.",
+      "Suitable for all harmonica enthusiasts and any beginner",
+      "This Professional Blues Harmonica has exquisite workmanship, beautiful appearance, accurate tone and responsive.This blues harmonica can be played with guitars, violins, flutes and more musical instrument.",
+    ],
+  },
+];
+
+export default products;
